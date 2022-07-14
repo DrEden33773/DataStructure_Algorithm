a404bd30e8de9ee91da881b9921748063f3d2f75
DrEden33773 <eden01960@nuaa.edu.cn>
2022-07-14 11:28:20 +0800

    feat: find the same birthday problem

---
+++ Algorithm/Match_The_Same_Birthday.hpp
@@ -0,0 +1,20 @@
+﻿/**
+ * @file Match_The_Same_Birthday.hpp
+ * @author Eden (edwardwang33773@gmail.com)
+ * @brief {
+        0. pre_definations => class birthday { int month; int day; };
+        1. found those who have the same birthday
+        2. return all <date:birthday, vector<name:string>> pairs
+    }
+ * @version 0.1
+ * @date 2022-07-14
+ *
+ * @copyright Copyright (c) 2022
+ *
+ */
+
+#include <iostream>
+#include <string>
+#include <unordered_map>
+#include <vector>
+using namespace std;
